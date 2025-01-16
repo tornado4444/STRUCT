fac8962ce7f61e34823c070c5d19afab488e1dd5
Tornado <tornadokiller4444@gmail.com>
2025-01-16 22:47:37 +0200

    Add files via upload

---
+++ Initialize.cpp
@@ -0,0 +1,105 @@
+﻿/*
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄
+▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌
+ ▀▀▀▀▀█░█▀▀▀ ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀      ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌     ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░▌          ▐░█▀▀▀▀▀▀▀▀▀
+	  ▐░▌    ▐░▌       ▐░▌▐░▌               ▐░▌          ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌               ▐░▌     ▐░▌          ▐░▌
+	  ▐░▌    ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░▌          ▐░▌          ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌     ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     ▐░▌          ▐░█▄▄▄▄▄▄▄▄▄
+	  ▐░▌    ▐░▌       ▐░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌     ▐░▌     ▐░▌          ▐░░░░░░░░░░░▌
+	  ▐░▌    ▐░▌       ▐░▌ ▀▀▀▀▀▀▀▀▀█░▌     ▐░▌          ▐░▌          ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀      ▐░█▀▀▀▀▀▀▀▀▀      ▐░▌     ▐░▌          ▐░█▀▀▀▀▀▀▀▀▀
+	  ▐░▌    ▐░▌       ▐░▌          ▐░▌     ▐░▌          ▐░▌          ▐░▌          ▐░▌               ▐░▌               ▐░▌     ▐░▌          ▐░▌
+ ▄▄▄▄▄█░▌    ▐░█▄▄▄▄▄▄▄█░▌ ▄▄▄▄▄▄▄▄▄█░▌     ▐░▌          ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌          ▐░▌               ▐░▌           ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄
+▐░░░░░░░▌    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░░░░░░░░░░░▌▐░▌          ▐░▌               ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+ ▀▀▀▀▀▀▀      ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀            ▀                 ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀
+
+*/
+
+#include "Initialize.hpp"
+
+// THE TEXT FILE
+bool Players::textFile(const char* filename) {
+	textfiles.open(filename);
+	if (!textfiles.is_open()) {
+		std::cout << "ERROR TO CREATE FILES!";
+		return false;
+	}
+	return true;
+}
+
+void Players::myInitialize() {
+	for (int i = 0; i < amplua; i++) {
+		std::cout << "Write the surname to the footballer " << i + 1 << ": ";
+		std::cin >> play[i].surname;
+
+		// DO THIS, IF THE CIN WHAT THE PLAYER'S AGE LESS THAN 18
+		do {
+			std::cout << "Write the age: ";
+			std::cin >> play[i].age;
+
+			if (play[i].age < 18)
+				std::cout << "THE PLAYER'S AGE, MUST BE OVER 18!!!" << std::endl;
+		} while (play[i].age < 18);
+		std::cout << "Write the count games: ";
+		std::cin >> play[i].count_games;
+		std::cout << "Write the count goals: ";
+		std::cin >> play[i].count_goals;
+
+		// WRITE THIS FILES
+		if (textfiles.is_open()) {
+			textfiles << "Player " << i + 1 << ":\n"
+				<< "Surname: " << play[i].surname << "\n"
+				<< "Age: " << play[i].age << "\n"
+				<< "Games: " << play[i].count_games << "\n"
+				<< "Goals: " << play[i].count_goals << "\n"
+				<< "------------------------\n";
+		}
+	}
+}
+
+void Players::lessPlaying() {
+	std::cout << "Players who have played fewer games: ";
+	if (textfiles.is_open()) {
+		textfiles << "\nPlayers who have played fewer games: ";
+	}
+
+	bool found = false;
+	for (int i = 0; i < amplua; i++) {
+		if (play[i].count_games < 5) {
+			if (found) {
+				std::cout << ", ";
+				if (textfiles.is_open())
+					textfiles << ", ";
+			}
+			std::cout << play[i].surname;
+			if (textfiles.is_open())
+				textfiles << play[i].surname;
+			found = true;
+		}
+	}
+
+	std::cout << std::endl;
+}
+
+// SEARCH THE BEST PLAYER
+void Players::theBestPlayer() {
+	int maxGoals = play[0].count_goals;
+	int bestPlayer = 0;
+
+	// SEARCH THE BEST PLAYER
+	for (int i = 1; i < amplua; i++) {
+		if (play[i].count_goals > maxGoals) {
+			maxGoals = play[i].count_goals;
+			bestPlayer = i;
+		}
+	}
+
+	std::cout << "\nThe best striker is: " << play[bestPlayer].surname
+		<< "\nGoals: " << play[bestPlayer].count_goals
+		<< "\nGames: " << play[bestPlayer].count_games
+		<< "\nAge: " << play[bestPlayer].age << std::endl;
+
+	// WRITE ON TEXTFILES
+	textfiles << "\nThe best striker is: " << play[bestPlayer].surname
+		<< "\nGoals: " << play[bestPlayer].count_goals
+		<< "\nGames: " << play[bestPlayer].count_games
+		<< "\nAge: " << play[bestPlayer].age << std::endl;
+}
---
+++ Initialize.hpp
@@ -0,0 +1,50 @@
+﻿/*
+ ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄
+▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌
+▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌     ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░▌          ▐░█▀▀▀▀▀▀▀▀▀
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌               ▐░▌     ▐░▌          ▐░▌
+▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌     ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     ▐░▌          ▐░█▄▄▄▄▄▄▄▄▄
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌     ▐░▌     ▐░▌          ▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀      ▐░█▀▀▀▀▀▀▀▀▀      ▐░▌     ▐░▌          ▐░█▀▀▀▀▀▀▀▀▀
+▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌               ▐░▌     ▐░▌          ▐░▌
+▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌           ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄
+▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+ ▀         ▀  ▀            ▀                 ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀
+
+*/
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+using std::ofstream;
+
+// the struct of Footballers, for initialize for GLOBAL the footbalers arguments
+struct Footballers {
+	std::string surname;
+	signed age;
+	int count_games;
+	int count_goals;
+};
+
+// THE STRUCT FOR INITIALIZE THE PLAYERS FOR SEARCH ARGUMENTS
+struct Players {
+	// AMLUA(SIZE)
+	static const int amplua = 5;
+
+	Footballers play[amplua]; // massive the footballers
+	ofstream textfiles; // for the write .txt files(THE FILE THREAT)
+	void myInitialize();
+	bool textFile(const char* filename);
+	void lessPlaying();
+	/*void theBestPlayer() const;*/ // NOT CORRECT, BECAUSE WE SPECIFY THIS FUNCTION FURTHER DOWN
+	void theBestPlayer();
+
+	// DESTRUCTOR FOR CLOSE THE FILE
+	~Players() {
+		if (textfiles.is_open()) { // For the open File
+			textfiles.close(); // just close the file
+		}
+	}
+};
